ab1a6aa3c0cca8077c56761119f5052c958d8e17
HelderCastrillon <helder_947@hotmail.com>
2015-02-21 18:53:56 -0500

    Versión de arranque
    Versión de arranque

---
+++ app.js
@@ -0,0 +1,39 @@
+var appContractSDSC = angular.module("appContractSDSC", ['ngRoute','Dhis2Api','pascalprecht.translate','ui.bootstrap','d2Menu']);
+
+appContractSDSC.config(function($routeProvider) {
+ 
+	  $routeProvider.when('/supervision', {
+		    templateUrl: "modules/contract/supervisionView.html",
+		    controller: "supervisionController"
+		  });
+	  $routeProvider.when('/attachinformation', {
+		  	templateUrl: "modules/contract/attachinformationView.html",
+		  	controller: "metadataimportController"
+		  });
+	  $routeProvider.otherwise({
+	        redirectTo: '/'
+	  });   
+
+	});
+
+appContractSDSC.config(function ($translateProvider) {
+  
+	  $translateProvider.useStaticFilesLoader({
+          prefix: 'languages/',
+          suffix: '.json'
+      });
+	  
+	  $translateProvider.registerAvailableLanguageKeys(
+			    ['es', 'en'],
+			    {
+			        'en*': 'en',
+			        'es*': 'es',
+			        '*': 'en' // must be last!
+			    }
+			);
+	  
+	  $translateProvider.fallbackLanguage(['en']);
+	  $translateProvider.determinePreferredLanguage();
+	  //$translateProvider.use('es');
+	  
+});
