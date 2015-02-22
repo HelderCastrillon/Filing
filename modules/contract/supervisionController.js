409d67727b82c7b45a2b4387469923479badd1f8
HelderCastrillon <helder_947@hotmail.com>
2015-02-22 14:00:36 -0500

    Ajustes a contratos

--- modules/contract/supervisionController.js
+++ modules/contract/supervisionController.js
@@ -1,6 +1,6 @@
 appContractSDSC.controller('supervisionController', ["$scope",'$filter',"commonvariable", "$modal", function($scope, $filter,commonvariable,$modal) {
 	
-    
+  $scope.supervisor={name:'Helder Castrillon', id:'10290528'}  
    	 
 }]);
 
