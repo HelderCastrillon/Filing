ca9d286dd7bda5981931eca32b13b941b8e3cc71
heldercastrillon <helder_947@hotmail.com>
2015-03-13 11:47:25 -0500

    progress bar

--- modules/administrationcontract/administrationcontractController.js
+++ modules/administrationcontract/administrationcontractController.js
@@ -1,11 +1,44 @@
-appContractSDSC.controller('administrationcontractController', ['$scope', 'fileUpload','commonvariable', function($scope, fileUpload,commonvariable){
-	$scope.progressvalue=0;
+appContractSDSC.controller('administrationcontractController', ['$scope', 'fileUpload','commonvariable','$timeout', function($scope, fileUpload,commonvariable,$timeout){
 	$scope.uploadFile = function(){
 		var file = $scope.myFile;
 		$scope.infofile=angular.fromJson(file);
         var uploadUrl = "../../../upload/uploadFile";
         fileUpload.uploadFileToUrl(file, uploadUrl, $scope.infofile.name, "Contratos");
+        $scope.progress=0;
+        $scope.showinfo=false;
+        $scope.upactive="active";
+        updateprocess();
 	};
+	function updateprocess() {
+		switch (commonvariable.Fileupload.status){
+			case "success":
+				$scope.status="success";
+				$scope.progress=100;
+				$scope.showinfo=true;
+				$scope.upactive="";
+				break;
+			case "error":
+				$scope.status="error";
+				$scope.progress=1;
+				$scope.showinfo=true;
+				break;
+			default:
+				break;	
+	}
+				
+		if($scope.progress<30)
+			$scope.progress = $scope.progress+1;
+		if($scope.progress>=30 && $scope.progress<90)
+			$scope.progress = $scope.progress + 1/2;
+		if($scope.progress>=90 && $scope.progress<98)
+			$scope.progress = $scope.progress + 1/10;
+		if($scope.progress>=98 && $scope.progress<100)
+			$scope.progress = $scope.progress + 1/100;
+		if($scope.progress>=100)
+			$scope.progress = 100;
+		if($scope.progress<100 && commonvariable.Fileupload.status=="waiting")
+			$timeout(updateprocess, 50);
+      }
     $scope.loadfile=function(){
     	$scope.imgurl=commonvariable.Fileupload.url;
     };
